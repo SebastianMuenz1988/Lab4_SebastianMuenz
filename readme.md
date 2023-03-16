5c2597b4c118806b4d5b2a5a73e0de80b82d0917
SebastianMuenz1988 <sebastian.muenz.24@gmail.com>
2023-03-16 15:50:11 +0100

    update Grade 5

--- readme.md
+++ readme.md
@@ -17,13 +17,15 @@ You can log in with your usernname and password. If you are not registered jet y
 
 After successfully login:
 
-- If you are in the role "student" or "teacher" you will be dynamically redirected to you individual profile page.
-
 - If you logged in as admin you can access every page (also the profile pages)
 
-- If you are logged in as (role)"student1" or "student2" you will be redirected directly to the corresponding "role-page". As (role) "teacher" or "student" you will be directly redirected to you personal profile page but you can also access the corresponding "role-page" with links below in the profile page or manually with URL (https://localhost:5000/teacher resp. student). Note that teacher can also access the students "role-page" but not the idividual students profile page.
+- If you are logged in as (role)"student1" or "student2" you will be redirected directly to the corresponding Home Page".
+- As (role) "teacher" or "student" you will be directly redirected to you personal profile page which no one but you and role "admin" can access but you can also reach the corresponding Home-Page with links below in the profile page or manually with URL (https://localhost:5000/teacher resp. student). Note that teacher can also access the students Home Page" but not the idividual students profile page. (For this to work I added an extra Home Page for the role "student" which is not included in the demands for this task but it seems logical to have one)
 
 Register:
-You can register as teacher or student. You also need a username that is not already used and a valid password (capital and lowercase letters, digts, special character and min 8 characters).
+You can register as teacher or student. You also need a username that is not already used and a valid password (capital and lowercase letters, a digt, a special character and min 8 characters).
 
 If everything is valid you will be redirected to the identification for login.
+
+No access:
+If you are not allwoed to access a certain URL you will be redirected to the /identification. There will also an status code 401 before the redirection and an console log with error message.
--- server.js
+++ server.js
@@ -32,134 +32,144 @@ app.use((req, res, next) => {
 // Middleware function for identification
 function authenticateUser(req, res, next) {
   if (currentJWT == "") {
-    console.log("No access to this page with current role: ", currentRole);
-    res.redirect("/identify");
+    console.log("Unauthorized - No acces with this role: ", currentRole);
+    res.status(401).redirect("/identify");
   } else if (jwt.verify(currentJWT, process.env.TOKEN)) {
     next();
   } else {
-    res.sendStatus(401);
+    res.status(401).redirect("/identify");
   }
 }
 
 function authenticateStudent1(req, res, next) {
   if (currentJWT == "" || (currentRole !== "student1" && currentRole !== "teacher" && currentRole !== "admin")) {
-    console.log("No access to this page with current role: ", currentRole);
-    res.redirect("/identify");
+    console.log("Unauthorized - No acces with this role: ", currentRole);
+    res.status(401).redirect("/identify");
   } else if (jwt.verify(currentJWT, process.env.TOKEN)) {
     next();
   } else {
-    res.sendStatus(401);
+    res.status(401).redirect("/identify");
   }
 }
 
 function authenticateStudent2(req, res, next) {
   if (currentJWT == "" || (currentRole !== "student2" && currentRole !== "teacher" && currentRole !== "admin")) {
-    console.log("No access to this page with current role: ", currentRole);
-    res.redirect("/identify");
+    console.log("Unauthorized - No acces with this role: ", currentRole);
+    res.status(401).redirect("/identify");
   } else if (jwt.verify(currentJWT, process.env.TOKEN)) {
     next();
   } else {
-    res.sendStatus(401);
+    res.status(401).redirect("/identify");
   }
 }
 
 function authenticateStudent(req, res, next) {
   if (currentJWT == "" || (currentRole !== "student" && currentRole !== "teacher" && currentRole !== "admin")) {
-    console.log("No access to this page with current role: ", currentRole);
-    res.redirect("/identify");
+    console.log("Unauthorized - No acces with this role: ", currentRole);
+    res.status(401).redirect("/identify");
   } else if (jwt.verify(currentJWT, process.env.TOKEN)) {
     next();
   } else {
-    res.sendStatus(401);
+    res.status(401).redirect("/identify");
   }
 }
 
 function authenticateTeacher(req, res, next) {
   if (currentJWT == "" || (currentRole !== "teacher" && currentRole !== "admin")) {
-    console.log("No access to this page with current role: ", currentRole);
-    res.redirect("/identify");
+    console.log("Unauthorized - No acces with this role: ", currentRole);
+    res.status(401).redirect("/identify");
   } else if (jwt.verify(currentJWT, process.env.TOKEN)) {
     next();
   } else {
-    res.sendStatus(401);
+    res.status(401).redirect("/identify");
   }
 }
 
 function authenticateAdmin(req, res, next) {
   if (currentJWT == "" || currentRole !== "admin") {
-    console.log("No access to this page with current role: ", currentRole);
-    res.redirect("/identify");
+    console.log("Unauthorized - No acces with this role: ", currentRole);
+    res.status(401).redirect("/identify");
   } else if (currentJWT) {
     try {
       jwt.verify(currentJWT, process.env.TOKEN);
       next();
     } catch (error) {
-      res.sendStatus(401);
+      res.status(401).redirect("/identify");
     }
   } else {
-    console.log("No access to this page with current role: ", currentRole);
-    res.redirect("/identify");
+    console.log("Unauthorized - No acces with this role: ", currentRole);
+    res.status(401).redirect("/identify");
   }
 }
 
 function authenticateUsers(req, res, next) {
   if (currentJWT == "" || (currentID !== req.params.userID && currentRole !== "admin")) {
-    console.log("Your current ID does not match the requestesd Profile Page!");
-    res.redirect("/identify");
+    console.log("Unorotrized! Your current ID does not match the requestesd Profile Page!");
+    res.status(401).redirect("/identify");
   } else if (jwt.verify(currentJWT, process.env.TOKEN)) {
     next();
   } else {
-    res.sendStatus(401);
+    res.status(401).redirect("/identify");
   }
 }
 
 app.use(express.urlencoded({ extended: false })); //only accept key-value pairs with string values
 
 // Routes
+
+// GET student1
+app.get("/student1", authenticateStudent1, (req, res) => {
+  res.render("student1.ejs");
+});
+
+// GET student2
+app.get("/student2", authenticateStudent2, (req, res) => {
+  res.render("student2.ejs");
+});
+
+// GET student
+app.get("/student", authenticateStudent, (req, res) => {
+  res.render("student.ejs");
+});
+
+// GET teacher
+app.get("/teacher", authenticateTeacher, (req, res) => {
+  res.render("teacher.ejs");
+});
+
+// GET granetd (In DB & valid password)
+app.get("/granted", authenticateUser, (req, res) => {
+  if (currentRole === "admin") res.redirect("/admin");
+  if (currentRole === "student1" || currentRole === "student2") res.redirect("/" + currentRole);
+  if (currentRole === "student" || currentRole === "teacher") res.redirect("/users/" + currentID); // the enpoint grandet has a JWT check middleware
+  else res.redirect("start.ejs");
+});
+
+// GET users
+app.get("/users/:userID", authenticateUsers, async (req, res) => {
+  let user = await database.getUserByID(req.params.userID);
+  res.render("user.ejs", { user: user });
+});
+
+// GET admin
+app.get("/admin", authenticateAdmin, async (req, res) => {
+  let allUsers = await database.getAllUsers();
+  res.render("admin.ejs", { users: allUsers });
+});
+
+// GET / --> Redirect to /identify
 app.get("/", (req, res) => {
   req.method = "GET";
   res.redirect("/identify");
   d;
 });
 
+// GET identify
 app.get("/identify", (req, res) => {
   res.render("identify.ejs");
 });
 
-app.get("/student1", authenticateStudent1, (req, res) => {
-  res.render("student1.ejs");
-});
-
-app.get("/student2", authenticateStudent2, (req, res) => {
-  res.render("student2.ejs");
-});
-
-app.get("/student", authenticateStudent, (req, res) => {
-  res.render("student.ejs");
-});
-
-app.get("/teacher", authenticateTeacher, (req, res) => {
-  res.render("teacher.ejs");
-});
-
-app.get("/granted", authenticateUser, (req, res) => {
-  if (currentRole === "admin") res.redirect("/admin");
-  if (currentRole === "student1" || currentRole === "student2") res.redirect("/" + currentRole);
-  if (currentRole === "student" || currentRole === "teacher") res.redirect("/users/" + currentID); // the enpoint grandet has a JWT check middleware
-  else res.redirect("start.ejs");
-  // res.render("start.ejs");
-});
-
-app.get("/users/:userID", authenticateUsers, async (req, res) => {
-  let user = await database.getUserByID(req.params.userID);
-  res.render("user.ejs", { user: user });
-});
-
-app.get("/admin", authenticateAdmin, async (req, res) => {
-  let allUsers = await database.getAllUsers();
-  res.render("admin.ejs", { users: allUsers });
-});
-
+// POST Identify
 app.post("/identify", async (req, res) => {
   console.log("req.body: ", req.body);
 
@@ -210,25 +220,30 @@ app.post("/identify", async (req, res) => {
   }
 });
 
-// app.post(“/register”) --> save user data in db (password encrypted)
+// GET Register - app.get("/register")--> render register.ejs
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+// POST Register
 app.post("/register", async (req, res) => {
   // Check req.body
   if (!isValidUserObject(req.body)) {
     console.log("Bad user input!");
-    res.status(401).redirect("/register");
+    res.status(400).redirect("/register");
     return;
   }
 
   const { name, role, password } = req.body; //Destructuring the req.body
 
   if (!isValidPassword(password)) {
-    res.status(401).redirect("/register");
+    res.status(400).redirect("/register");
     return;
   }
 
   if (await database.getUserByName(name)) {
     console.log("There is already a User with the name: ", name, "!");
-    res.status(401).redirect("/register");
+    res.status(400).redirect("/register");
     return;
   }
 
@@ -244,11 +259,8 @@ app.post("/register", async (req, res) => {
   // Define new id
   let allUsers = await database.getAllUsers();
   nextID = "id" + allUsers.length;
-  console.log("nextID", nextID);
-  console.log("name", name);
 
   // Save new User in DB
-  // console.log("add user: ", name, role, passwordEncrypted);
   let status = await database.addUser(nextID, name, role, passwordEncrypted);
   console.log(status);
   //res.sendStatus(201); //.send("new user registered!");
@@ -258,9 +270,5 @@ app.post("/register", async (req, res) => {
   res.redirect("/identify");
 });
 
-// Route 5 - app.get("/register")--> render register.ejs
-app.get("/register", (req, res) => {
-  res.render("register.ejs");
-});
-
+// Server listen
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
